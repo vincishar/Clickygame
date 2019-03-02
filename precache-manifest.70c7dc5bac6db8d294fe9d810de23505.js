c28e983c4cd2706f1be736ee5d2f5fc75ce74eb7
vbhardwaj93@gmail.com <vbhardwaj93@gmail.com>
2019-03-02 09:12:11 -0500

    Updates

---
+++ precache-manifest.70c7dc5bac6db8d294fe9d810de23505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "047f1f917bc80522b345",
+    "url": "/Clickygame/static/css/main.baeb3dfd.chunk.css"
+  },
+  {
+    "revision": "047f1f917bc80522b345",
+    "url": "/Clickygame/static/js/main.047f1f91.chunk.js"
+  },
+  {
+    "revision": "9c9af4e9a89f957943b5",
+    "url": "/Clickygame/static/js/runtime~main.9c9af4e9.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Clickygame/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "43fef914f6c929dce8f872431bb0c58d",
+    "url": "/Clickygame/index.html"
+  }
+];
